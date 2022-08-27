1db6ea3c0e6177ff4e57ca4570000d1aa0f50777
RahmanEmon <48353914+RahmanEmon@users.noreply.github.com>
2022-08-27 12:33:10 +0600

    Data Updated

--- Global_SuperShop_Project/transactionTable.sql
+++ Global_SuperShop_Project/transactionTable.sql
@@ -1,3 +1,3 @@
 SELECT 
-	Row_ID, Order_ID, Customer_ID, Product_ID, Sales, Quantity, Sales_per_Unit, Discount, Profit, Profit_per_Unit, Costs, Cost_Per_Unit, Shipping_Cost, ShippingCost_per_Unit  
+	Row_ID, Order_ID, City, Customer_ID, Product_ID, Sales, Quantity, Sales_per_Unit, Discount, Profit, Profit_per_Unit, Costs, Cost_Per_Unit, Shipping_Cost, ShippingCost_per_Unit  
 FROM storedata;
